bd87db3ff328b3fb4f957b987ca42f1cc560943e
zamy <zamy@cris.ac.th>
2024-10-23 15:19:51 +0700

    Fix code

--- america.py
+++ america.py
@@ -1,5 +1,5 @@
 '''
-<Zamy>, <Grade 9> <Intro to Computer Science>
+Zamy, Grade 9 Intro to Computer Science
 Assignment: String Slicing
 This assignment will demonstrate how to manipulate and print strings.
 '''
@@ -18,7 +18,7 @@ print(country.upper())
 
 # 4. print the length of the country string as:
 #    string length of country: <length>
-print(f"string length of country:", (len(country)))
+print("string length of country:", (len(country)))
 
 # 5. slice the string "america" from country, upper case the 'a',
 #    and store it in a variable named `short_name` with a value "America"
@@ -30,9 +30,8 @@ print(short_name)
 #    and print the result:
 #    <title case country> is located within <title case continent>
 continent = "north america"
-print(f"{short_name} is located within {continent.title()}")
+print(f"{country.title()} is located within {continent.title()}")
 
 # 7. print a statement how we refer to people from the country:
 #    People from <title case country> are called <title case of people in plural>
-people_from_america = "americans"
-print(f"People from {country.title()} are called {people_from_america.title()}")
+print(f"People from {country.title()} are called {short_name.title() + "ns"}")
